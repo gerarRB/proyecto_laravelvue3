a06a8388b0c15c1bec5b880f68b951a148dd86fc
gerarRB <gerarbarriostm@gmail.com>
2024-11-06 17:27:15 -0600

    cambios en AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Forzar el uso de HTTPS en URLs generadas en producción
+        if (config('app.env') !== 'local') {
+            URL::forceScheme('https');
+        }
     }
 }
+
