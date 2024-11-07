3833d1f91de9a5270eaf8c09addf726cfc1b7458
gerarRB <gerarbarriostm@gmail.com>
2024-11-06 20:11:41 -0600

    cambios en bootstrap.js y app.blade.php

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -7,7 +7,16 @@
 import axios from 'axios';
 window.axios = axios;
 
+window.axios.defaults.withCredentials = true; // Asegura que las cookies se envíen con la solicitud
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+const token = document.head.querySelector('meta[name="csrf-token"]');
+
+if (token) {
+    window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
+} else {
+    console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
+}
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
