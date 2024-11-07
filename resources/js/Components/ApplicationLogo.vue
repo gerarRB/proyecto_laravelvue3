8a535a391396c7791fb4897957268f133c0f02c7
gerarRB <gerarbarriostm@gmail.com>
2024-11-06 21:46:29 -0600

    imagen Logo

--- resources/js/Components/ApplicationLogo.vue
+++ resources/js/Components/ApplicationLogo.vue
@@ -1,4 +1,13 @@
 <template>
-    <img src="/build/assets/logo.png" alt="Logo ULS" style="width: 170px; height: auto;">
+    <img :src="logoUrl" alt="Logo ULS" style="width: 170px; height: auto;">
 </template>
 
+<script>
+export default {
+    data() {
+        return {
+            logoUrl: import.meta.env.VITE_LOGO_URL
+        }
+    }
+}
+</script>
