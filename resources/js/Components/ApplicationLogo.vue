91c35163b5ee5a0e70067ee7c90000f7a4cfa90d
gerarRB <gerarbarriostm@gmail.com>
2024-11-06 21:55:27 -0600

    regresar imagen

--- resources/js/Components/ApplicationLogo.vue
+++ resources/js/Components/ApplicationLogo.vue
@@ -1,6 +1,4 @@
-
 <template>
-    <img :src="`${import.meta.env.VITE_PUBLIC_URL}/build/assets/logo.png`" alt="Logo ULS" style="width: 170px; height: auto;">
-  </template>
-  
-  
+    <img src="/build/assets/logo.png" alt="Logo ULS" style="width: 170px; height: auto;">
+</template>
+
