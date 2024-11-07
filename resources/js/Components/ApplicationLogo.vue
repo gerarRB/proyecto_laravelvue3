b4a710c60ed2c2ab9f436b77ee0bfc460272e69f
gerarRB <gerarbarriostm@gmail.com>
2024-11-06 22:08:52 -0600

    logo svg

--- resources/js/Components/ApplicationLogo.vue
+++ resources/js/Components/ApplicationLogo.vue
@@ -1,4 +1,10 @@
 <template>
-    <img src="/build/assets/logo.png" alt="Logo ULS" style="width: 170px; height: auto;">
-</template>
+    <div class="flex items-center">
+      <!-- Ícono SVG -->
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="text-blue-500 w-8 h-8">
+        <path fill-rule="evenodd" d="M4.293 6.293a1 1 0 0 1 1.414 0L12 12.586l6.293-6.293a1 1 0 0 1 1.414 1.414L13.414 14l6.293 6.293a1 1 0 0 1-1.414 1.414L12 15.414l-6.293 6.293a1 1 0 0 1-1.414-1.414L10.586 14 4.293 7.707a1 1 0 0 1 0-1.414z" clip-rule="evenodd" />
+      </svg>
+    </div>
+  </template>
+  
 
